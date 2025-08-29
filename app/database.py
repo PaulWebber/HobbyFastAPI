16c8e1839ed559c84756fe88a77a0f47e8f0380d
Paul Webber <paulw@tehnerdz.com>
2025-08-29 12:33:15 -0600

    290825_1232 Now uses SQLite. Added 1 Hobby

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app/hobby.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'hobby.db')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
