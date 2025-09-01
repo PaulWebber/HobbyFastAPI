9af26975009d95555d30a997ba39e7854da06c25
Paul Webber <paulw@tehnerdz.com>
2025-09-01 14:53:44 -0600

    010925_1453 Want to clean up that Add Option

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -62,4 +62,25 @@ test('Add Fields to Hobby', async ({ page }) => {
   await page.getByRole('button', { name: 'Add', exact: true }).click();
 });
 
+test('Make sure all fields show up', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/');
+  await page.getByRole('button', { name: 'Slingshot' }).click();
+  await expect(page.getByRole('button', { name: 'Back' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Config' })).toBeVisible();
+  await expect(page.getByText('Name:')).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Name' })).toBeVisible();
+  await expect(page.getByText('Qty:')).toBeVisible();
+  await expect(page.getByPlaceholder('Qty')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Add Option' })).toBeVisible();
+  await expect(page.getByRole('combobox')).toBeVisible();
+  await expect(page.getByText('Usable:')).toBeVisible();
+  await expect(page.getByRole('checkbox')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Save Item' })).toBeVisible();
+  await expect(page.locator('#itemFields')).toContainText('Name:');
+  await expect(page.locator('#itemFields')).toContainText('Qty:');
+  await expect(page.locator('#itemFields')).toContainText('Latex:');
+  await expect(page.locator('#itemFields')).toContainText('Usable:');
+  await expect(page.locator('#saveItemBtn')).toContainText('Save Item');
+});
+
 // await expect(page.locator('input')).not.toHaveAttribute('disabled', '');
